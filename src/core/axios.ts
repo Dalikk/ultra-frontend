fbcfbb3d866dce4e55b3ec5c5e3fede501c2a17a
Dalikk <dav@sciberia.io>
2023-05-03 12:37:35 +0900

    add axios

---
+++ src/core/axios.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const axiosClient = axios.create();
+
+axiosClient.defaults.baseURL = process.env.API_URL || 'http://localhost:5001';
+
+// При каждом запросе
+// axiosClient.interceptors.request.use((config) => {
+//   // Делаем проверку т.к. Next работает как на клиенте, так и на сервере
+//   if (typeof window !== 'undefined') {
+//     const { _token } = parseCookies();
+//     config.headers.Authorization = 'Bearer ' + _token;
+//   }
+//   return config;
+// });
+
+export default axiosClient;
