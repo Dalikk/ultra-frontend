b2f805696561c7864160a628dacbce5b3252eab2
Dalikk <dav@sciberia.io>
2023-05-03 14:55:58 +0900

    add api key to axios

--- src/core/axios.ts
+++ src/core/axios.ts
@@ -5,13 +5,13 @@ const axiosClient = axios.create();
 axiosClient.defaults.baseURL = process.env.API_URL || 'http://localhost:5001';
 
 // При каждом запросе
-// axiosClient.interceptors.request.use((config) => {
-//   // Делаем проверку т.к. Next работает как на клиенте, так и на сервере
-//   if (typeof window !== 'undefined') {
-//     const { _token } = parseCookies();
-//     config.headers.Authorization = 'Bearer ' + _token;
-//   }
-//   return config;
-// });
+axiosClient.interceptors.request.use((config) => {
+  // Делаем проверку т.к. Next работает как на клиенте, так и на сервере
+  // if (typeof window !== 'undefined') {
+  //   const { _token } = parseCookies();
+  //   config.headers.Authorization = 'Bearer ' + _token;
+  config.headers['x-api-key'] = process.env.API_KEY;
+  return config;
+});
 
 export default axiosClient;
